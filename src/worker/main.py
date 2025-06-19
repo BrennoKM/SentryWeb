92dc2da1fa7832e47a4ddb945ad55a723a29bf13
Brenno Kevyn <brennokm@gmail.com>
2025-06-19 11:01:59 -0300

    🧐 Mexendo no HPA #6

--- src/worker/main.py
+++ src/worker/main.py
@@ -59,14 +59,14 @@ def callback(ch, method, properties, body):
 def listen_for_tasks():
     global executor
     log("[worker] [INFO] Criando novo executor...")
-    executor = ThreadPoolExecutor(max_workers=20)
+    executor = ThreadPoolExecutor(max_workers=30)
 
     connection = start_consumer(
         callback=callback,
         queue='tasks',
         durable=True,
         auto_ack=False,
-        prefetch_count=20,
+        prefetch_count=30,
         manual_ack_inside_callback=True
     )
 
