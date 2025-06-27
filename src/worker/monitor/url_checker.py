86c4853182e462d4598c0a112b351acef2122db5
Brenno Kevyn <brennokm@gmail.com>
2025-06-27 19:25:51 -0300

    🧐 Mexendo no HPA #14

--- src/worker/monitor/url_checker.py
+++ src/worker/monitor/url_checker.py
@@ -1,8 +1,11 @@
 import requests
+import time
 
 def check_url(url):
     try:
-        with requests.get(url, timeout=5) as response:
-            return response.status_code == 200
+        time.sleep(1.45)
+        return True
+        # with requests.get(url, timeout=5) as response:
+        #     return response.status_code == 200
     except Exception:
         return False
